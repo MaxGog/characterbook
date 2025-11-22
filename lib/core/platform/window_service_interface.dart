c443683f442181ca39e7b2ab96bbc39cc7770207
MaxGog <max.gog2005@outlook.com>
2025-11-22 19:20:36 +0300

    Откат назад к рабочей версии менеджера окна

--- lib/core/platform/window_service_interface.dart
+++
@@ -1,9 +0,0 @@
-abstract class WindowServiceInterface {
-  Future<void> initialize();
-  Future<void> minimize();
-  Future<void> toggleMaximize();
-  Future<void> close();
-  Future<bool> isMaximized();
-  void addListener(dynamic listener);
-  void removeListener(dynamic listener);
-}
